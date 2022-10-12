bcbeb3e93a263b21b47a0bfe9d4d9e10ebeeb1ca
Susan Dwyer <suzycream999@googlemail.com>
2022-10-12 09:34:55 +0100

    Add project files.

---
+++ HowTo_Hotkeys_Winforms/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HowTo_Hotkeys_Winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Escape: ExitApp(); return true;
+                case Keys.Right: Next(); return true;
+                case Keys.Left: Previous(); return true;
+                case Keys.NumPad0: Previous(); return true;
+                default: return base.ProcessCmdKey(ref msg, keyData);
+            }
+        }
+
+        private void Previous() { MessageBox.Show("Previous"); }
+
+        private void Next() { MessageBox.Show("Next"); }
+
+        private void ExitApp() { MessageBox.Show("Exit"); }
+    }
+}
